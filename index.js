3ef79a6123ad3354ba78050011da0c497c76b6b5
tthygesen <tobias.ekenberg@gmail.com>
2018-12-30 02:43:05 +0100

    first

--- index.js
+++ index.js
@@ -34,7 +34,14 @@ dbConnection();
 
 app.use("/", routes);
 
-const port = 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, err => {
   if (err) {
     console.log(err);
